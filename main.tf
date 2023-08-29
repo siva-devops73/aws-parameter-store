b719be7f08c1aa750d75834964df8c445b0c9589
Siva Kumar <sivakumar9399959803@gmail.com>
2023-08-29 11:46:47 +0530

    aws-parameter-store

--- main.tf
+++ main.tf
@@ -19,8 +19,13 @@ variable "params" {
     { name = "roboshop.dev.cart.redis_host", value = "redis-dev.devops73.store", type = "String" },
     { name = "roboshop.dev.cart.catalogue_host", value = "catalogue-dev.devops73.store", type = "String" },
     { name = "roboshop.dev.cart.catalogue_port", value = "8080", type = "String" },
+
     { name = "roboshop.dev.catalogue.mongo", value = "true", type = "String" },
-    { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://mongodb-dev.devops73.store:27017/catalogue", type = "String" },
+    #password involved in catalogie
+    { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cjvkzflxlvkt.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
+    { name = "roboshop.dev.catalogue.docdb_endpoint", value = "docdb-dev.cluster-cjvkzflxlvkt.us-east-1.docdb.amazonaws.com", type = "String" },
+
+
     { name = "roboshop.dev.payment.cart_host", value = "cart-dev.devops73.store", type = "String" },
     { name = "roboshop.dev.payment.cart_port", value = "8080", type = "String" },
     { name = "roboshop.dev.payment.user_host", value = "user-dev.devops73.store", type = "String" },
